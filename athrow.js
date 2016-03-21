018a81cc5fbc828f1b848127126734080c0d3b5b
Dan Ross <ross9885@gmail.com>
2016-03-21 17:03:48 +0300

    reject, don't throw

--- athrow.js
+++ athrow.js
@@ -2,10 +2,11 @@ async function test() {
 	try {
 		await new Promise((resolve, reject) => {
 			setTimeout(() => {
-				throw new Error("Can't touch this!");
+				reject(new Error("Can't touch this!"));
 			}, 1);
 		});
 	} catch (e) {
+		console.log("Failed await");
 		throw new Error("fail to win ftw");
 	}
 }
